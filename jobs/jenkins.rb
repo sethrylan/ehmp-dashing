6417e624a0225977dbf46f81734a26c66bec18cb
Seth Rylan Gainey <seth.rylan@gmail.com>
2014-03-14 23:31:40 -0400

    Hardcoded portions of api/json extract.

--- jobs/jenkins.rb
+++ jobs/jenkins.rb
@@ -1,36 +1,43 @@
-jenkins_host = 'https://build.vistacore.us'
-img_path = '/static/12429cfa/images/48x48/'
-port = 80
+jenkins_host = 'build.vistacore.us'
+img_path = '/static/foo/images/48x48/'
+port = 443
 
 last_builds = {}
 
 SCHEDULER.every '10s', :first_in => 0 do |foo|
   http = Net::HTTP.new(jenkins_host, port)
-  response = http.request(Net::HTTP::Get.new("/api/json?depth=1"))
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+  response = http.request(Net::HTTP::Get.new("/api/json"))
   jobs = JSON.parse(response.body)["jobs"]
 
   builds = []
 
-  jobs.map! do |job|
+  jobs.select{|j| j['name']=~/ehmp/}.map! do |job|
     name = job['name']
-    cov_path = "/job/#{name}/lastBuild/cobertura/api/json?depth=2"
-    response = http.request(Net::HTTP::Get.new(cov_path))
-    coverage = nil
-    if response.code == '200'
-      elements = JSON.parse(response.body)['results']['elements']
-      elements.map! do |element|
-        if element['name'] == 'Conditionals'
-          coverage = element['ratio']
-        end
-      end
-    end
+    # cov_path = "/job/#{name}/lastBuild/cobertura/api/json?depth=2"
+    # response = http.request(Net::HTTP::Get.new(cov_path))
+    # coverage = nil
+    # if response.code == '200'
+    #   elements = JSON.parse(response.body)['results']['elements']
+    #   elements.map! do |element|
+    #     if element['name'] == 'Conditionals'
+    #       coverage = element['ratio']
+    #     end
+    #   end
+    # end
+    coverage = 0.20
 
-    color = job['color'].sub('blue', 'green')
+    color = job['color'].gsub('blue', 'green').gsub('disabled', 'grey')
+    icon = job['color'].gsub('disabled', 'grey')
+
     status = case color
+               when 'red' then 'Failed'
                when 'green' then 'Success'
                when 'yellow' then 'Unstable'
                when 'disabled' then 'Disabled'
-               when 'gray' then 'Disabled'
+               when 'grey' then 'Disabled'
                when 'aborted' then 'Aborted'
                when 'green_anime' then 'Building'
                when 'red_anime' then 'Building'
@@ -39,9 +46,13 @@ SCHEDULER.every '10s', :first_in => 0 do |foo|
                when 'yellow_anime' then 'Building'
                else 'Failure'
              end
-    icon_url = job['healthReport'][0]['iconUrl']
-    health_url = "http://#{jenkins_host}:#{port}#{img_path}#{icon_url}"
-    desc = job['description']
+    # status = 'Success'
+    # icon_url = job['healthReport'][0]['iconUrl']
+    # health_url = "#{jenkins_host}:#{port}#{img_path}#{icon_url}"
+    health_url = "https://build.vistacore.us/static/12429cfa/images/48x48/#{icon}.png"
+    # desc = job['description']
+    desc = 'Description'
+
     build = {
       name: name, status: status, health: health_url, color: color
     }
